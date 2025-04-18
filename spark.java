cb6d6eac8d01c88bbcf1ceefa1a3687f6d3015a5
Abhay Thakare <at7888143196@gmail.com>
2025-04-19 02:27:59 +0530

    change in spark file

--- spark.java
+++ spark.java
@@ -2,5 +2,10 @@ class spark {
     public static void main(String args[])
     {
         System.out.println("Hello World");
+        m1();
+    }
+    public static void  m1()
+    {
+        System.out.println("M1");
     }
 }
